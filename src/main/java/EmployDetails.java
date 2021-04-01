2942681389907eff1a58cac5462069c1a9285d16
Spero <marvinhope070@gmail.com>
2021-04-01 21:24:08 +0200

    Updated version 1.2

--- src/main/java/EmployDetails.java
+++ src/main/java/EmployDetails.java
@@ -9,4 +9,34 @@
  * Marvin Hope - 219445842 
  */
 public class EmployDetails {
+    private String EmployID, EmployNum, EmpSur;
+
+    public String getEmployID() {
+        return EmployID;
+    }
+
+    public void setEmployID(String EmployID) {
+        this.EmployID = EmployID;
+    }
+
+    public String getEmployNum() {
+        return EmployNum;
+    }
+
+    public void setEmployNum(String EmployNum) {
+        this.EmployNum = EmployNum;
+    }
+
+    public String getEmpSur() {
+        return EmpSur;
+    }
+
+    public void setEmpSur(String EmpSur) {
+        this.EmpSur = EmpSur;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "EmployID=" + EmployID + ", EmployNum=" + EmployNum + ", EmpSur=" + EmpSur + '}';
+    }
 }
